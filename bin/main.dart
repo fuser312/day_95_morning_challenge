bdeb58b3ae69c22788096e7729a19fa7fc49328a
fuser312 <sherltonGT40000>
2020-03-13 10:55:41 +0530

    Done day 95 challenge.

--- bin/main.dart
+++ bin/main.dart
@@ -41,5 +41,86 @@
 // string "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or
 // ['bedbath', 'and', 'beyond'].
 
-void main() {
+
+int longestStreak(List<Map<String, String>> myList){
+  int count = 0;
+  int prevCount = 0;
+  myList.sort((a, b) => (b["date"]).compareTo(a["date"]));
+  List<Map<String, String>> newList = (myList.reversed.toList());
+  print(newList);
+  print(DateTime.parse("2019-09-18"));
+  if(myList.isEmpty){
+    return 0;
+  }
+  if(myList.length == 1) {
+    return 1;
+  }
+
+  for(int i = 0; i< newList.length - 1; i++ ){
+    print(DateTime.parse(newList[i]["date"]).difference(DateTime.parse(newList[i+1]["date"])));
+   if( DateTime.parse(newList[i]["date"]).difference(DateTime.parse(newList[i+1]["date"])).toString() == "-24:00:00.000000"){
+
+     count++;
+   }
+   else {
+     if(count > prevCount){
+       prevCount = count;
+       count = 0;
+     }
+   }
+  }
+  print (prevCount+1);
+  return prevCount+1;
 }
+
+
+List <String> reconstruction(List<String> words, String str ){
+
+  if(str.isEmpty){
+    return [];
+  }
+  for(int i=1; i<=str.length; i++){
+    String testString=str.substring(0,i);
+
+    if(words.contains(testString)){
+      List<String> result = reconstruction(words, str.substring(i));
+      if(result != null){
+         result.add(testString);
+         return result;
+      }
+    }
+  }
+
+  return null;
+
+}
+
+
+
+void main() {
+  longestStreak([
+    {
+      "date": "2019-09-18"
+    },
+    {
+      "date": "2019-09-19"
+    },
+    {
+      "date": "2019-09-20"
+    },
+    {
+      "date": "2019-09-26"
+    },
+    {
+      "date": "2019-09-27"
+    },
+    {
+      "date": "2019-09-30"
+    }
+  ]);
+
+  print(reconstruction( ['quick', 'brown', 'the', 'fox'], "thequickbrownfox",));
+
+}
+
+
